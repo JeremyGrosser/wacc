4b41dba94d29c9c259c2bec37ff287ed313849d5
Jeremy Grosser <jeremy@synack.me>
2024-07-26 20:14:52 -0700

    initial

---
+++ listings/return_2.s
@@ -0,0 +1,10 @@
+	.file	"return_2.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+	movl	$2, %eax
+	ret
+	.size	main, .-main
+	.ident	"GCC: (Debian 12.2.0-14) 12.2.0"
+	.section	.note.GNU-stack,"",@progbits

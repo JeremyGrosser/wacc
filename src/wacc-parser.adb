1c68775c3ce6cbddb18bcfda154f457b129ab624
Jeremy Grosser <jeremy@synack.me>
2024-07-28 23:45:52 -0700

    exp grammar

--- src/wacc-parser.adb
+++ src/wacc-parser.adb
@@ -23,37 +23,62 @@ package body WACC.Parser is
          end if;
       end Expect;
 
-      function Parse_Exp
-         return WACC.AST.Any_Exp_Node
+      procedure Parse_Unop
+         (Node : in out WACC.AST.Any_Unary_Operator_Node)
       is
          Tok : constant WACC.Lexer.Token := First_Element (Input);
-         Node : WACC.AST.Any_Exp_Node;
       begin
-         if Tok.Typ = WACC.Lexer.T_int then
-            Node := new WACC.AST.Exp_Node'(Typ => WACC.AST.N_Constant, Int => 0);
-            for Ch of Ada.Strings.Unbounded.To_String (Tok.Literal) loop
-               if Ch not in '0' .. '9' then
-                  raise Program_Error with "Expected 0 .. 9 in int constant, got '" & Ch & "'";
+         case Tok.Typ is
+            when WACC.Lexer.T_Dash =>
+               Node := new WACC.AST.Unary_Operator_Node'(Typ => WACC.AST.N_Negate);
+            when WACC.Lexer.T_Tilde =>
+               Node := new WACC.AST.Unary_Operator_Node'(Typ => WACC.AST.N_Complement);
+            when others =>
+               raise Parse_Error with "Expected Dash or Tilde token in unary operator, got " & Tok.Typ'Image;
+         end case;
+         Delete_First (Input);
+      end Parse_Unop;
+
+      procedure Parse_Exp
+         (Node : in out WACC.AST.Any_Exp_Node)
+      is
+         Tok : constant WACC.Lexer.Token := First_Element (Input);
+      begin
+         case Tok.Typ is
+            when WACC.Lexer.T_int =>
+               Node := new WACC.AST.Exp_Node'(Typ => WACC.AST.N_Constant, Int => 0);
+               for Ch of Ada.Strings.Unbounded.To_String (Tok.Literal) loop
+                  if Ch not in '0' .. '9' then
+                     raise Program_Error with "Expected 0 .. 9 in int constant, got '" & Ch & "'";
+                  end if;
+                  Node.Int := Node.Int * 10 + Character'Pos (Ch) - Character'Pos ('0');
+               end loop;
+               Delete_First (Input);
+            when WACC.Lexer.T_Dash | WACC.Lexer.T_Tilde =>
+               Node := new WACC.AST.Exp_Node'(Typ => WACC.AST.N_Unary, others => <>);
+               Parse_Unop (Node.Unary_Operator);
+               Parse_Exp (Node.Exp);
+            when WACC.Lexer.T_Open_Paren =>
+               Delete_First (Input);
+               Parse_Exp (Node);
+               if Tok.Typ = WACC.Lexer.T_Close_Paren then
+                  Delete_First (Input);
+               else
+                  raise Parse_Error with "Expected ')' after exp";
                end if;
-               Node.Int := Node.Int * 10 + Character'Pos (Ch) - Character'Pos ('0');
-            end loop;
-            Delete_First (Input);
-            return Node;
-         else
-            raise Parse_Error with "Expected Constant in exp";
-         end if;
+            when others =>
+               raise Parse_Error with "Unexpected token in exp: " & Tok.Typ'Image;
+         end case;
       end Parse_Exp;
 
-      function Parse_Statement
-         return WACC.AST.Any_Statement_Node
+      procedure Parse_Statement
+         (Node : in out WACC.AST.Any_Statement_Node)
       is
-         Node : WACC.AST.Any_Statement_Node;
       begin
          Expect (WACC.Lexer.T_return);
          Node := new WACC.AST.Statement_Node'(Typ => WACC.AST.N_Return, Exp => null);
-         Node.Exp := Parse_Exp;
+         Parse_Exp (Node.Exp);
          Expect (WACC.Lexer.T_Semicolon);
-         return Node;
       end Parse_Statement;
 
       procedure Parse_Function
@@ -71,7 +96,7 @@ package body WACC.Parser is
          Expect (WACC.Lexer.T_void);
          Expect (WACC.Lexer.T_Close_Paren);
          Expect (WACC.Lexer.T_Open_Brace);
-         Node.FBody := Parse_Statement;
+         Parse_Statement (Node.FBody);
          Expect (WACC.Lexer.T_Close_Brace);
       end Parse_Function;
    begin

a256711fc5e86d9d31c13edd5cf40f21cbd7020a
Jeremy Grosser <jeremy@synack.me>
2024-10-03 17:56:09 -0700

    forward declarations for wacc.tacky

--- src/wacc-tacky.ads
+++ src/wacc-tacky.ads
@@ -3,9 +3,32 @@ with WACC.Strings; use WACC.Strings;
 with WACC.AST;
 
 package WACC.TACKY is
+   type Function_Definition_Node;
+   type Any_Function_Definition_Node is access Function_Definition_Node;
+   type Instruction_Node;
+   type Any_Instruction_Node is access Instruction_Node;
+   type Val_Node;
+   type Any_Val_Node is access Val_Node;
+   type Unary_Operator_Node;
+   type Any_Unary_Operator_Node is access Unary_Operator_Node;
+   type Binary_Operator_Node;
+   type Any_Binary_Operator_Node is access Binary_Operator_Node;
+
+   package Instruction_Node_Vectors is new Ada.Containers.Vectors
+      (Index_Type   => Positive,
+       Element_Type => Any_Instruction_Node);
+
+   --  function_definition = Function(identifier, 1 instruction* body)
+   type Function_Definition_Node is record
+      Name  : Identifier;
+      FBody : Instruction_Node_Vectors.Vector;
+   end record;
 
    --  program = Program(function_definition)
-   --  function_definition = Function(identifier, 1 instruction* body)
+   type Program_Node is record
+      Function_Definition : Function_Definition_Node;
+   end record;
+
    --  instruction = Return(val)
    --              | Unary(unary_operator, val src, val dst)
    --              | Binary(binary_operator, val src1, val src2, val dst)
@@ -14,53 +37,6 @@ package WACC.TACKY is
    --              | JumpIfZero(val condition, identifier target)
    --              | JumpIfNotZero(val condition, identifier target)
    --              | Label(identifier)
-   --  val = Constant(int) | Var(identifier)
-   --  unary_operator = Complement | Negate | Not
-   --  binary_operator = Add | Subtract | Multiply | Divide | Remainder | Equal
-   --                  | NotEqual | LessThan | LessOrEqual | GreaterThan
-   --                  | GreaterOrEqual
-
-   type Binary_Operator_Type is
-      (TA_Add,
-       TA_Subtract,
-       TA_Multiply,
-       TA_Divide,
-       TA_Remainder,
-       TA_Equal,
-       TA_Not_Equal,
-       TA_Less_Than,
-       TA_Less_Or_Equal,
-       TA_Greater_Than,
-       TA_Greater_Or_Equal);
-   type Binary_Operator_Node
-      (Typ : Binary_Operator_Type)
-   is null record;
-   type Any_Binary_Operator_Node is access Binary_Operator_Node;
-
-   type Unary_Operator_Type is
-      (TA_Complement,
-       TA_Negate,
-       TA_Not);
-   type Unary_Operator_Node
-      (Typ : Unary_Operator_Type)
-   is null record;
-   type Any_Unary_Operator_Node is access Unary_Operator_Node;
-
-   type Val_Node_Type is
-      (TA_Constant,
-       TA_Var);
-   type Val_Node
-      (Typ : Val_Node_Type)
-   is record
-      case Typ is
-         when TA_Constant =>
-            Int : Long_Integer;
-         when TA_Var =>
-            Name : Identifier;
-      end case;
-   end record;
-   type Any_Val_Node is access Val_Node;
-
    type Instruction_Node_Type is
       (TA_Return,
        TA_Unary,
@@ -96,19 +72,49 @@ package WACC.TACKY is
             Label : Identifier;
       end case;
    end record;
-   type Any_Instruction_Node is access Instruction_Node;
 
-   package Instruction_Node_Vectors is new Ada.Containers.Vectors
-      (Positive, Any_Instruction_Node);
-
-   type Function_Definition_Node is record
-      Name  : Identifier;
-      FBody : Instruction_Node_Vectors.Vector;
+   --  val = Constant(int) | Var(identifier)
+   type Val_Node_Type is
+      (TA_Constant,
+       TA_Var);
+   type Val_Node
+      (Typ : Val_Node_Type)
+   is record
+      case Typ is
+         when TA_Constant =>
+            Int : Long_Integer;
+         when TA_Var =>
+            Name : Identifier;
+      end case;
    end record;
 
-   type Program_Node is record
-      Function_Definition : Function_Definition_Node;
-   end record;
+   --  unary_operator = Complement | Negate | Not
+   type Unary_Operator_Type is
+      (TA_Complement,
+       TA_Negate,
+       TA_Not);
+   type Unary_Operator_Node
+      (Typ : Unary_Operator_Type)
+   is null record;
+
+   --  binary_operator = Add | Subtract | Multiply | Divide | Remainder | Equal
+   --                  | NotEqual | LessThan | LessOrEqual | GreaterThan
+   --                  | GreaterOrEqual
+   type Binary_Operator_Type is
+      (TA_Add,
+       TA_Subtract,
+       TA_Multiply,
+       TA_Divide,
+       TA_Remainder,
+       TA_Equal,
+       TA_Not_Equal,
+       TA_Less_Than,
+       TA_Less_Or_Equal,
+       TA_Greater_Than,
+       TA_Greater_Or_Equal);
+   type Binary_Operator_Node
+      (Typ : Binary_Operator_Type)
+   is null record;
 
    procedure Generate
       (Tree : WACC.AST.Program_Node;
@@ -116,5 +122,4 @@ package WACC.TACKY is
 
    procedure Print
       (Node : Program_Node);
-
 end WACC.TACKY;
